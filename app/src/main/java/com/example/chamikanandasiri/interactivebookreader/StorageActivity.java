6e72dc12e6fb98a584133b7708b1b327418e54cd
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-21 21:24:33 +0530

    search methods added

--- app/src/main/java/com/example/chamikanandasiri/interactivebookreader/StorageActivity.java
+++ app/src/main/java/com/example/chamikanandasiri/interactivebookreader/StorageActivity.java
@@ -1,7 +1,14 @@
 package com.example.chamikanandasiri.interactivebookreader;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +17,12 @@ public class StorageActivity extends AppCompatActivity {
     DataBaseHelper dataBaseHelper;
     CommentHandler commentHandler;
     WordHandler wordHandler;
+    ListView str_listView;
+    EditText str_searchText;
+    ImageButton str_searchButton;
+
+    ArrayList<WordObject> wordObjects;
+    ArrayList<CommentObject> commentObjects;
 
 
     @Override
@@ -21,6 +34,118 @@ public class StorageActivity extends AppCompatActivity {
         commentHandler = new CommentHandler(dataBaseHelper, this);
         wordHandler = new WordHandler(dataBaseHelper, this);
 
-        Log.d("Test", wordHandler.getWords().toString());
+        str_listView = findViewById(R.id.StorageListView);
+        str_searchText = findViewById(R.id.StorageEditText);
+        str_searchButton = findViewById(R.id.StorageSearchButton);
+
+        String type = getIntent().getStringExtra("type");
+
+        if (type.equals("word")) {
+            str_searchText.setHint("search by word");
+            str_searchButton.setOnClickListener(v -> {
+                buttonAnimation1(str_searchButton);
+                searchWord(v);
+            });
+            loadAllWords();
+        } else {
+            str_searchText.setHint("search by Title");
+            str_searchButton.setOnClickListener(v -> {
+                buttonAnimation1(str_searchButton);
+                searchTitle(v);
+            });
+            loadAllComments();
+        }
+    }
+
+    public void searchTitle(View v1) {
+        String q = str_searchText.getText().toString();
+        if (!q.equals("")) {
+            commentObjects = new ArrayList<>();
+            ArrayList<String> titles = commentHandler.getSimilarTitles(q);
+            for (String title : titles) {
+                ArrayList<String[]> phrcmt = commentHandler.getPhraseCommentbyTitle(title);
+                StringBuilder phrases = new StringBuilder();
+                StringBuilder comments = new StringBuilder();
+                for (String[] a : phrcmt) {
+                    phrases.append(a[0]).append("\n");
+                    comments.append(a[1]).append("\n");
+                }
+                commentObjects.add(new CommentObject(title, phrases.toString(), comments.toString()));
+            }
+
+            commentListGenerate();
+
+        }
+    }
+
+    public void searchWord(View v1) {
+        String q = str_searchText.getText().toString();
+        if (!q.equals("")) {
+            wordObjects = new ArrayList<>();
+            ArrayList<String> words = wordHandler.getSimilarWords(q);
+            for (String word : words) {
+                ArrayList<String[]> defpos = wordHandler.getDefinitionPosByWord(word);
+                StringBuilder definitions = new StringBuilder();
+                StringBuilder POSs = new StringBuilder();
+                for (String[] a : defpos) {
+                    definitions.append(a[1]).append("\n");
+                    POSs.append(a[0]).append("\n");
+                }
+                wordObjects.add(new WordObject(word, definitions.toString(), POSs.toString()));
+            }
+
+            wordListGenerate();
+        }
+    }
+
+    private void loadAllWords() {
+        wordObjects = new ArrayList<>();
+        ArrayList<String> words = wordHandler.getWords();
+        for (String word : words) {
+            ArrayList<String[]> defpos = wordHandler.getDefinitionPosByWord(word);
+            StringBuilder definitions = new StringBuilder();
+            StringBuilder POSs = new StringBuilder();
+            for (String[] a : defpos) {
+                definitions.append(a[1]).append("\n");
+                POSs.append(a[0]).append("\n");
+            }
+            wordObjects.add(new WordObject(word, definitions.toString(), POSs.toString()));
+        }
+
+        wordListGenerate();
+    }
+
+    private void loadAllComments() {
+        commentObjects = new ArrayList<>();
+        ArrayList<String> titles = commentHandler.getAllDistinctTitles();
+        for (String title : titles) {
+            ArrayList<String[]> phrcmt = commentHandler.getPhraseCommentbyTitle(title);
+            StringBuilder phrases = new StringBuilder();
+            StringBuilder comments = new StringBuilder();
+            for (String[] a : phrcmt) {
+                phrases.append(a[0]).append("\n");
+                comments.append(a[1]).append("\n");
+            }
+            commentObjects.add(new CommentObject(title, phrases.toString(), comments.toString()));
+        }
+
+        commentListGenerate();
+    }
+
+    private void wordListGenerate() {
+        WordArrayAdapter adapter = new WordArrayAdapter(this, R.layout.word_card, wordObjects);
+        str_listView.setAdapter(adapter);
+    }
+
+    private void commentListGenerate() {
+        CommentArrayAdapter adapter = new CommentArrayAdapter(this, R.layout.comment_card, commentObjects);
+        str_listView.setAdapter(adapter);
+    }
+
+    private void buttonAnimation1(ImageButton button) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        BounceInterpolator bi = new BounceInterpolator(0.2, 20);
+        animation.setInterpolator(bi);
+        button.startAnimation(animation);
     }
 }
