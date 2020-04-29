88c5f7ec52b360d3a6bb6252c758dc64b3a7c29e
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-29 10:59:59 +0530

    displaying the saved books from db

--- app/src/main/java/com/example/chamikanandasiri/interactivebookreader/LibraryActivity.java
+++ app/src/main/java/com/example/chamikanandasiri/interactivebookreader/LibraryActivity.java
@@ -1,14 +1,13 @@
 package com.example.chamikanandasiri.interactivebookreader;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -16,26 +15,54 @@ public class LibraryActivity extends AppCompatActivity {
     private EditText lib_searchText;
     private GridView lib_gridView;
 
-    private String TAG ="Test";
+    private DataBaseHelper dataBaseHelper;
+    private BookHandler bookHandler;
+
+    private String TAG = "Test";
+
+    private ArrayList<SimpleBookObject> displayingBooks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
-        BookObject book = (BookObject)getIntent().getSerializableExtra("Book");
-        ArrayList<BookObject> booksTest = new ArrayList<>();
-        booksTest.add(book);
-        booksTest.add(book);
-        booksTest.add(book);
-        booksTest.add(book);
-        booksTest.add(book);
-
+        dataBaseHelper = new DataBaseHelper(this);
+        bookHandler = new BookHandler(dataBaseHelper,this);
+        displayingBooks = new ArrayList<>();
         lib_gridView = findViewById(R.id.LibraryGridView);
         lib_searchButton = findViewById(R.id.LibrarySearchButton);
         lib_searchText = findViewById(R.id.LibraryEditText);
 
-        BookArrayAdapter adapter = new BookArrayAdapter(this,R.layout.listitem_book,booksTest);
+        loadBookDetails();
+
+//        BookObject book = (BookObject)getIntent().getSerializableExtra("Book");
+//        ArrayList<BookObject> booksTest = new ArrayList<>();
+//        booksTest.add(book);
+//        booksTest.add(book);
+//        booksTest.add(book);
+//        booksTest.add(book);
+//        booksTest.add(book);
+
+
+
+    }
+
+    private void loadBookDetails() {
+        ArrayList<String> bookIDs = bookHandler.getAllBookIDs();
+        for (String id : bookIDs) {
+            ArrayList<String> bookdet = bookHandler.getBooksByID(id);
+            String title = bookdet.get(0);
+            String auth = bookdet.get(1);
+            String isbn = bookdet.get(2);
+            String img = bookdet.get(3);
+            displayingBooks.add(new SimpleBookObject(id,title.toUpperCase(), auth, isbn, img));
+        }
+        loadView();
+    }
+
+    private void loadView() {
+        SimpleBookArrayAdapter adapter = new SimpleBookArrayAdapter(this, R.layout.listitem_book, displayingBooks);
         lib_gridView.setAdapter(adapter);
     }
 
