6c4e87befea50a70e1527fe2308f7aa13a1d14b8
chamikaCN <chamikanandasiri97@gmail.com>
2020-05-12 16:34:45 +0530

    new fields added

--- app/src/main/java/com/example/chamikanandasiri/interactivebookreader/DownloadContentObject.java
+++ app/src/main/java/com/example/chamikanandasiri/interactivebookreader/DownloadContentObject.java
@@ -1,6 +1,9 @@
 package com.example.chamikanandasiri.interactivebookreader;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DownloadContentObject implements Serializable {
 
@@ -9,17 +12,30 @@ public class DownloadContentObject implements Serializable {
     private String name;
     private String description;
     private String size;
+    private String bookID;
+    private Timestamp timestamp;
     private String fileURL;
+    private int timeStampUniqueCount;
 
     private String TAG ="Test";
 
-    public DownloadContentObject(String id, String[] imageURLs, String name, String description, String size, String fileURL) {
+    public DownloadContentObject(String id, String[] imageURLs, String name, String bookID, String description, String size, String fileURL, int timeCount) {
         this.id = id;
         this.imageURLs = imageURLs;
         this.name = name;
         this.description = description;
         this.size = size;
+        this.bookID =bookID;
         this.fileURL = fileURL;
+        this.timeStampUniqueCount = timeCount;
+        this.timestamp = addTimeStamp();
+    }
+
+    private Timestamp addTimeStamp() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        String stamp = formatter.format(date) +  "." + timeStampUniqueCount;
+        return Timestamp.valueOf(stamp);
     }
 
     public String getContId() {
@@ -40,6 +56,14 @@ public class DownloadContentObject implements Serializable {
 
     public String getContSize() {
         return size;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public String getTimestamp() {
+        return timestamp.toString();
     }
 
     public String getFileURL() {
