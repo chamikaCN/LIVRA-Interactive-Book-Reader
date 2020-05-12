1028ff744b532b6ef161498ee39c771f1b32e5db
chamikaCN <chamikanandasiri97@gmail.com>
2020-05-12 16:31:21 +0530

    content saveing to database added

---
+++ app/src/main/java/com/example/chamikanandasiri/interactivebookreader/ContentHandler.java
@@ -0,0 +1,51 @@
+package com.example.chamikanandasiri.interactivebookreader;
+
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
+public class ContentHandler {
+
+    private DataBaseHelper dbHelper;
+    private Context context;
+
+    private String TAG = "Test";
+
+    public ContentHandler(DataBaseHelper dbHelper, Context context) {
+        this.dbHelper = dbHelper;
+        this.context = context;
+    }
+
+    public ArrayList<String[]> getContentsByBookID(String bookID) {
+        Cursor res = dbHelper.getContentDetailsByBooKID(bookID);
+        ArrayList<String[]> results = new ArrayList<>();
+
+        while (res.moveToNext()) {
+            String Ac = res.getString(0);
+            String Bc = res.getString(1);
+            String Cc = res.getString(2);
+            String Dc = res.getString(3);
+
+//            String[] cat = new String[]{Ac,Bc};
+//            Log.d(TAG, "getPhraseCommentByTitle: "+ Arrays.toString(cat));
+            results.add(new String[]{Ac,Bc,Cc,Dc});
+
+        }return results;
+    }
+
+    public ArrayList<String> getContentIDsByBookIDs(String id) {
+        Cursor res = dbHelper.getContentIDsByBooKID(id);
+        ArrayList<String> results = new ArrayList<>();
+
+        while (res.moveToNext()) {
+            results.add(res.getString(0));
+        }
+        return results;
+    }
+
+    public boolean addContent(DownloadContentObject contentObject) {
+        return dbHelper.insertRowContent(contentObject.getContId(), contentObject.getTimestamp(), contentObject.getBookID(),
+                contentObject.getContName(), contentObject.getContSize(), contentObject.getImageURLs()[0], contentObject.getFileURL());
+    }
+}
---
+++ app/src/main/java/com/example/chamikanandasiri/interactivebookreader/SimpleContentObject.java
@@ -0,0 +1,42 @@
+package com.example.chamikanandasiri.interactivebookreader;
+
+import java.io.Serializable;
+
+public class SimpleContentObject implements Serializable {
+
+    private String id;
+    private String imageURL;
+    private String name;
+    private String bookID;
+    private String fileURL;
+
+    private String TAG ="Test";
+
+    public SimpleContentObject(String id, String imageURL, String name, String bookID, String fileURL) {
+        this.id = id;
+        this.imageURL = imageURL;
+        this.name = name;
+        this.bookID = bookID;
+        this.fileURL = fileURL;
+    }
+
+    public String getContId() {
+        return id;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public String getContName() {
+        return name;
+    }
+
+    public String getContentBookID() {
+        return bookID;
+    }
+
+    public String getFileURL() {
+        return fileURL;
+    }
+}
