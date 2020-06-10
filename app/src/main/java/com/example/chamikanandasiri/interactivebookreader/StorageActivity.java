dfc35df4c91b2f918e8fe4762d2d00959d5e9025
chamikaCN <chamikanandasiri97@gmail.com>
2020-06-10 21:01:52 +0530

    on back pressed method added

--- app/src/main/java/com/example/chamikanandasiri/interactivebookreader/StorageActivity.java
+++ app/src/main/java/com/example/chamikanandasiri/interactivebookreader/StorageActivity.java
@@ -1,7 +1,9 @@
 package com.example.chamikanandasiri.interactivebookreader;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -160,4 +162,12 @@ public class StorageActivity extends AppCompatActivity {
         animation.setInterpolator(bi);
         button.startAnimation(animation);
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
 }
